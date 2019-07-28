7042d196eb1d99b9cb4ef33b2532e2417b201ab1
hangfeifu <5724924@qq.com>
2019-07-28 15:23:55 +0800

    use sql orm for adding paper,question,option finished!

--- survey/survey/settings.py
+++ survey/survey/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'survey',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,14 @@ WSGI_APPLICATION = 'survey.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'survey',
+        'HOST':'localhost',
+        'PORT':3306,
+        'USER':'root',
+        'PASSWORD':'rootroot'
+
+
     }
 }
 
--- survey/survey/views.py
+++ survey/survey/views.py
@@ -1,13 +1,25 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
+import json
+
+from survey.models import *
+
+
 def addView(request):
     return render(request, "add.html")
 def erro(request):
     return render(request, "404.html")
+
 def addSuccess(request):
-    try:
-        if request.POST['username']=='sb110':
-            return render(request, "success.html")
-        return render(request, "404.html")
-    except:
-        return render(request, "404.html")
+    paper_obj=Paper(name=request.POST.get('paperName'),
+                    detail=request.POST.get('paperDetail'))
+    paper_obj.save()
+    for i in json.loads( request.POST.get('questions')):
+        question_obj=Question(no=i['no'],content=i['questionName'],type=i['type'],ismustfill=i['ismustfill'],pid=paper_obj)
+        question_obj.save()
+        count=1
+        for k in i['option']:
+            option_obj=Option(no=count,content=k,qid=question_obj)
+            count+=1
+            option_obj.save()
+    return JsonResponse({'resultCode':0})
